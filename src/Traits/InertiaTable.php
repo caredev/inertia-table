0f7ff18e9b0a0db10aef3a5e2c5c258b3cd64239
Care <71512870+caredev@users.noreply.github.com>
2021-08-30 11:46:56 +0200

    Update InertiaTable.php

--- src/Traits/InertiaTable.php
+++ src/Traits/InertiaTable.php
@@ -2,10 +2,10 @@
 
 namespace harmonic\InertiaTable\Traits;
 
-trait InertiaTable
-{
-    public function resolveRouteBinding($value)
-    {
+use Illuminate\Support\Str;
+
+trait InertiaTable {
+    public function resolveRouteBinding($value) {
         return in_array(SoftDeletes::class, class_uses($this))
             ? $this->where($this->getRouteKeyName(), $value)->withTrashed()->first()
             : parent::resolveRouteBinding($value);
@@ -19,9 +19,21 @@ trait InertiaTable
      * @param string $order asc or desc
      * @return void
      */
-    public function scopeOrder($query, $column, $order = 'asc')
-    {
-        $query->orderBy($column, $order !== 'asc' ? 'desc' : 'asc');
+    public function scopeOrder($query, $column, $order = 'asc') {
+        if (Str::contains($column, ',')) {
+            $columns = explode(',', $column);
+            foreach ($columns as $column) {
+                preg_match_all('/(\w+)\s*(asc|desc)?/i', $column, $matches, PREG_SET_ORDER, 0);
+                if (!empty($matches)) {
+                    $column = $matches[0][1];
+                    $order = $matches[0][2] ?? $order;
+                }
+
+                $query->orderBy($column, $order !== 'asc' ? 'desc' : 'asc');
+            }
+        } else {
+            $query->orderBy($column, $order !== 'asc' ? 'desc' : 'asc');
+        }
     }
 
     /**
@@ -32,14 +44,13 @@ trait InertiaTable
      * @param array $searchColumns An array of column names that can be searched
      * @return void
      */
-    public function scopeFilter($query, array $filters, array $searchColumns)
-    {
+    public function scopeFilter($query, array $filters, array $searchColumns) {
         $query->when($filters['search'] ?? null, function ($query, $search) use ($searchColumns) {
             $query->where(function ($query) use ($search, $searchColumns) {
-                if (! empty($searchColumns)) {
-                    $query->where(array_shift($searchColumns), 'like', '%'.$search.'%');
+                if (!empty($searchColumns)) {
+                    $query->where(array_shift($searchColumns), 'like', '%' . $search . '%');
                     foreach ($searchColumns as $columnName) {
-                        $query->orWhere($columnName, 'like', '%'.$search.'%');
+                        $query->orWhere($columnName, 'like', '%' . $search . '%');
                     }
                 }
             });
